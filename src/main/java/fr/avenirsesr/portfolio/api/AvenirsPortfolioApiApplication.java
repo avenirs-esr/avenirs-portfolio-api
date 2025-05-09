cb471f3ed9917bc3c6375d88ba79d366fce0ea42
Mouhamed Diallo <mouhamed.diallo409@gmail.com>
2025-05-09 09:29:23 +0200

    Project initialization

---
+++ src/main/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package fr.avenirsesr.portfolio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvenirsPortfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvenirsPortfolioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.avenirsesr.portfolio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvenirsPortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
