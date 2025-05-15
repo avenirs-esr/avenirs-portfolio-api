4c345933304d8f6b8906e3dc8ff57e0de7b525f2
Nathan Pignon <58686941+Nathan-Pignon@users.noreply.github.com>
2025-05-15 12:22:46 +0200

    chore(ci): Reusable workflow implementation (#2)
    * chore(ci): test reusable workflow for backend project
    
    * fix(ci): fix url of distant workflow
    
    * fix(ci): url adapted to comply with github workflow references
    
    fix(ci): url adapted to comply with github workflow references
    
    fix(ci): url adapted to comply with github workflow references
    
    * refactor(api): re-formatting files
    
    refactor(api): re-formatting files with google java format
    
    * feat(tests): add jacoco plugin
    
    * feat(test): application.properties in tests to execute TUs
    
    feat(test): application.properties in tests to execute TUs
    
    fix(core): test with branch parameter
    
    fix(core): test with branch parameter
    
    fix(ci): test without parameters
    
    * fix(core): update bdd test url
    
    * fix(core): update target branch in common-backend-workflow call

--- src/main/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplication.java
+++ src/main/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AvenirsPortfolioApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AvenirsPortfolioApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AvenirsPortfolioApiApplication.class, args);
+  }
 }
--- src/test/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplicationTests.java
+++ src/test/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplicationTests.java
@@ -2,12 +2,12 @@ package fr.avenirsesr.portfolio.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AvenirsPortfolioApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
