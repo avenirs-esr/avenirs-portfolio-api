e2edb00eb37afc0ab5f85bb1726abeed043d9aff
Nathan <nathan.pignon79@gmail.com>
2025-06-05 12:00:47 +0200

    fix(test): enabled seeder in application-test properties

--- src/test/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplicationTests.java
+++ src/test/java/fr/avenirsesr/portfolio/api/AvenirsPortfolioApiApplicationTests.java
@@ -1,6 +1,5 @@
 package fr.avenirsesr.portfolio.api;
 
-import fr.avenirsesr.portfolio.api.infrastructure.adapter.repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
