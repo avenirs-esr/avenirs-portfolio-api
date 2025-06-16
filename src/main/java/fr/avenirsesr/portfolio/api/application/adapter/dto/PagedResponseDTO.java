b4c0b7964bb8793d9bd8fa38f1fe727c4ea8900d
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-06-16 15:11:08 +0200

    feat: pagniation dtos

---
+++ src/main/java/fr/avenirsesr/portfolio/api/application/adapter/dto/PagedResponseDTO.java
@@ -0,0 +1,10 @@
+package fr.avenirsesr.portfolio.api.application.adapter.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+@Schema(requiredProperties = {"data", "page"})
+public record PagedResponseDTO<T>(
+    @Schema(description = "List of elements on the current page") List<T> data,
+    @Schema(description = "Pagination information") PageDTO page) {
+}
