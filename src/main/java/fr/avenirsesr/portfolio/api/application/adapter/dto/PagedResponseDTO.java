f0f8d3a12ea8d387808a9943c1018aa0e1dac295
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-06-16 16:29:49 +0200

    fix: field renamed

--- src/main/java/fr/avenirsesr/portfolio/api/application/adapter/dto/PagedResponseDTO.java
+++ src/main/java/fr/avenirsesr/portfolio/api/application/adapter/dto/PagedResponseDTO.java
@@ -3,8 +3,8 @@ package fr.avenirsesr.portfolio.api.application.adapter.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-@Schema(requiredProperties = {"data", "page"})
+@Schema(requiredProperties = {"content", "page"})
 public record PagedResponseDTO<T>(
-    @Schema(description = "List of elements on the current page") List<T> data,
+    @Schema(description = "List of elements on the current page") List<T> content,
     @Schema(description = "Pagination information") PageDTO page) {
 }
