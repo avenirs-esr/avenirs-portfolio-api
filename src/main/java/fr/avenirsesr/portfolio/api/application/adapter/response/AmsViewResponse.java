95515d7134ba73f5877d32bf110e3a5210933027
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-06-17 10:41:13 +0200

    refactor: paged response and fix models loacation

---
+++ src/main/java/fr/avenirsesr/portfolio/api/application/adapter/response/AmsViewResponse.java
@@ -0,0 +1,12 @@
+package fr.avenirsesr.portfolio.api.application.adapter.response;
+
+import fr.avenirsesr.portfolio.api.application.adapter.dto.AmsViewDTO;
+import fr.avenirsesr.portfolio.api.domain.model.PaginationInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+@Schema(description = "Paginated response containing AMS view information")
+public record AmsViewResponse(
+    List<AmsViewDTO> content,
+    PaginationInfo page) implements PagedResponse<AmsViewDTO> {
+}
