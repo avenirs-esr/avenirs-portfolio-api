5dfce0a88f7b30c6f2d0fb33c2c78d2927982d0c
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-06-20 12:05:51 +0200

    fix: name in open api spec

--- src/main/java/fr/avenirsesr/portfolio/api/application/adapter/dto/AmsProgressDTO.java
+++ src/main/java/fr/avenirsesr/portfolio/api/application/adapter/dto/AmsProgressDTO.java
@@ -3,7 +3,7 @@ package fr.avenirsesr.portfolio.api.application.adapter.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Random;
 
-@Schema(requiredProperties = {"startedActivities", "totalActivities"})
+@Schema(name = "AMSProgress", requiredProperties = {"startedActivities", "totalActivities"})
 public record AmsProgressDTO(int startedActivities, int totalActivities) {
 
   public static AmsProgressDTO createNotStartedMock() {
