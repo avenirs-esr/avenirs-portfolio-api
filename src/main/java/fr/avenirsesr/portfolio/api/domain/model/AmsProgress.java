53125ee9cd3c8c772de56da40ee1c00dc00ad288
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-06-20 09:10:33 +0200

    fix: formatting

--- src/main/java/fr/avenirsesr/portfolio/api/domain/model/AmsProgress.java
+++ src/main/java/fr/avenirsesr/portfolio/api/domain/model/AmsProgress.java
@@ -5,13 +5,13 @@ import java.util.Random;
 
 @Schema(requiredProperties = {"validated", "total", "percentage"})
 public record AmsProgress(int validated, int total, int percentage) {
-  
+
   public static AmsProgress createMock() {
     Random random = new Random();
     int total = random.nextInt(8) + 1;
     int validated = random.nextInt(total + 1);
     int percentage = (int) Math.round(((double) validated / total) * 100);
-    
+
     return new AmsProgress(validated, total, percentage);
   }
 }
