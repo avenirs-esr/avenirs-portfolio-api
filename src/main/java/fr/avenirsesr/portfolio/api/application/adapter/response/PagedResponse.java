01e9063239315574aea76079af55d42d7c426258
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-06-23 13:58:40 +0200

    fix: invalid field name in @Schema annotation

--- src/main/java/fr/avenirsesr/portfolio/api/application/adapter/response/PagedResponse.java
+++ src/main/java/fr/avenirsesr/portfolio/api/application/adapter/response/PagedResponse.java
@@ -4,7 +4,7 @@ import fr.avenirsesr.portfolio.api.domain.model.PageInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-@Schema(requiredProperties = {"content", "page"})
+@Schema(requiredProperties = {"data", "page"})
 public interface PagedResponse<T> {
   @Schema(description = "List of elements on the current page")
   List<T> data();
