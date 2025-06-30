ed56016cb6523f66f56c147bac81ca1f38d25a53
Nathan <nathan.pignon79@gmail.com>
2025-06-30 16:47:41 +0200

    fix(376): lint and refacto security config test

--- src/main/java/fr/avenirsesr/portfolio/api/infrastructure/adapter/security/DevAuthenticationFilter.java
+++ src/main/java/fr/avenirsesr/portfolio/api/infrastructure/adapter/security/DevAuthenticationFilter.java
@@ -5,15 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
