326f7b54b746c48f1a648bff3654a1438ec38b60
Nathan <nathan.pignon79@gmail.com>
2025-07-25 16:39:18 +0200

    fix(342): reactivate integration tests and fix bugs

--- src/test/java/fr/avenirsesr/portfolio/program/application/adapter/controller/TrainingPathControllerIT.java
+++ src/test/java/fr/avenirsesr/portfolio/program/application/adapter/controller/TrainingPathControllerIT.java
@@ -1,3 +1,124 @@
 package fr.avenirsesr.portfolio.program.application.adapter.controller;
 
-public class TrainingPathControllerIT {}
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import fr.avenirsesr.portfolio.shared.domain.model.enums.ELanguage;
+import fr.avenirsesr.portfolio.shared.infrastructure.adapter.seeder.SeederRunner;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class TrainingPathControllerIT {
+
+  @Autowired private MockMvc mockMvc;
+
+  @Value("${hmac.secret-key}")
+  private String secretKey;
+
+  @Value("${user.student.payload}")
+  private String studentPayload;
+
+  @Value("${user.teacher.payload}")
+  private String teacherPayload;
+
+  @Value("${user.unknown.payload}")
+  private String unknownUserPayload;
+
+  @Value("${user.student.signature}")
+  private String studentSignature;
+
+  @Value("${user.teacher.signature}")
+  private String teacherSignature;
+
+  @Value("${user.unknown.signature}")
+  private String unknownUserSignature;
+
+  private ELanguage language = ELanguage.FRENCH;
+
+  @BeforeAll
+  static void setup(@Autowired SeederRunner seederRunner) {
+    seederRunner.run();
+  }
+
+  @Test
+  void shouldReturnAllProgramProgressForStudent() throws Exception {
+    mockMvc
+        .perform(
+            get("/me/training-paths")
+                .header("X-Signed-Context", studentPayload)
+                .header("X-Context-Kid", secretKey)
+                .header("X-Context-Signature", studentSignature)
+                .header("Accept-Language", language.getCode())
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$[0].id").value("64e9ef37-e847-467c-81e5-4d27d4cdf905"))
+        .andExpect(jsonPath("$[0].name").value("Eastern Doctoral - 1 [fr_FR]"))
+        .andExpect(jsonPath("$[0].durationUnit").value("YEAR"))
+        .andExpect(jsonPath("$[0].durationCount").value(1));
+  }
+
+  @Test
+  void shouldReturn404WhenUserIsUnknown() throws Exception {
+    mockMvc
+        .perform(
+            get("/me/training-paths")
+                .header("X-Signed-Context", unknownUserPayload)
+                .header("X-Context-Kid", secretKey)
+                .header("X-Context-Signature", unknownUserSignature)
+                .header("Accept-Language", language.getCode())
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.message").value("User not found"))
+        .andExpect(jsonPath("$.code").value("USER_NOT_FOUND"));
+  }
+
+  @Test
+  void shouldReturn403WhenUserIsNotStudentForAllProgramProgress() throws Exception {
+    mockMvc
+        .perform(
+            get("/me/training-paths")
+                .header("X-Signed-Context", teacherPayload)
+                .header("X-Context-Kid", secretKey)
+                .header("X-Context-Signature", teacherSignature)
+                .header("Accept-Language", language.getCode())
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.message").value("User is not student"))
+        .andExpect(jsonPath("$.code").value("USER_IS_NOT_STUDENT_EXCEPTION"));
+  }
+
+  @Test
+  void shouldFallbackInDefaultLanguageWhenLanguageNotSupportedForAllProgramProgress()
+      throws Exception {
+    mockMvc
+        .perform(
+            get("/me/training-paths")
+                .header("X-Signed-Context", studentPayload)
+                .header("X-Context-Kid", secretKey)
+                .header("X-Context-Signature", studentSignature)
+                .header("Accept-Language", "invalid_language_code")
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$[0].id").value("64e9ef37-e847-467c-81e5-4d27d4cdf905"))
+        .andExpect(jsonPath("$[0].name").value("Eastern Doctoral - 1 [fr_FR]"))
+        .andExpect(jsonPath("$[0].durationUnit").value("YEAR"))
+        .andExpect(jsonPath("$[0].durationCount").value(1));
+  }
+}
