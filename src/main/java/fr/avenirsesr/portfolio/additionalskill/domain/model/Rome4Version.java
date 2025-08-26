9942010ad11368188849319b2a1339fb7a2e5c12
Mouhamed Diallo <mouhamed.diallo409@gmail.com>
2025-08-26 08:19:29 +0200

    fix(additional-skill) : rebase develop

--- src/main/java/fr/avenirsesr/portfolio/additionalskill/domain/model/Rome4Version.java
+++ src/main/java/fr/avenirsesr/portfolio/additionalskill/domain/model/Rome4Version.java
@@ -1,5 +1,6 @@
 package fr.avenirsesr.portfolio.additionalskill.domain.model;
 
+import fr.avenirsesr.portfolio.shared.domain.model.AvenirsBaseModel;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.Getter;
@@ -7,22 +8,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Rome4Version {
+public class Rome4Version extends AvenirsBaseModel {
   private UUID id;
   private Integer version;
   private Instant lastModifiedDate;
 
-  private Rome4Version(UUID id, Integer version, Instant lastModifiedDate) {
-    this.id = id;
+  private Rome4Version(
+      UUID id, Instant createdAt, Instant updatedAt, Integer version, Instant lastModifiedDate) {
+    super(id, createdAt, updatedAt);
     this.version = version;
     this.lastModifiedDate = lastModifiedDate;
   }
 
   public static Rome4Version create(Integer version, Instant lastModifiedDate) {
-    return new Rome4Version(UUID.randomUUID(), version, lastModifiedDate);
+    Instant now = Instant.now();
+    return new Rome4Version(UUID.randomUUID(), now, now, version, lastModifiedDate);
   }
 
   public static Rome4Version toDomain(UUID id, Integer version, Instant lastModifiedDate) {
-    return new Rome4Version(id, version, lastModifiedDate);
+    Instant now = Instant.now();
+    return new Rome4Version(id, now, now, version, lastModifiedDate);
   }
 }
