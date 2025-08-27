8c55fb5e44cc7d54c6eab401b15edc48e4d5241f
bilelbanelhaq <bilel.banelhaq@gmail.com>
2025-08-27 16:09:47 +0200

    fix(additional-skills): dto were breaking the front

--- src/main/java/fr/avenirsesr/portfolio/additionalskill/application/adapter/dto/AdditionalSkillProgressDTO.java
+++ src/main/java/fr/avenirsesr/portfolio/additionalskill/application/adapter/dto/AdditionalSkillProgressDTO.java
@@ -1,10 +1,15 @@
 package fr.avenirsesr.portfolio.additionalskill.application.adapter.dto;
 
 import fr.avenirsesr.portfolio.additionalskill.domain.model.enums.EAdditionalSkillLevel;
+import fr.avenirsesr.portfolio.additionalskill.domain.model.enums.EAdditionalSkillType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.UUID;
 
 @Schema(requiredProperties = {"id", "title", "pathSegments", "type", "level"})
 public record AdditionalSkillProgressDTO(
-    UUID id, String title, List<String> pathSegments, String type, EAdditionalSkillLevel level) {}
+    UUID id,
+    String title,
+    List<String> pathSegments,
+    EAdditionalSkillType type,
+    EAdditionalSkillLevel level) {}
--- src/main/java/fr/avenirsesr/portfolio/additionalskill/application/adapter/mapper/AdditionalSkillProgressMapper.java
+++ src/main/java/fr/avenirsesr/portfolio/additionalskill/application/adapter/mapper/AdditionalSkillProgressMapper.java
@@ -14,7 +14,7 @@ public interface AdditionalSkillProgressMapper {
             additionalSkillProgress.getSkill().getPathSegments().getIssue().getLibelle(),
             additionalSkillProgress.getSkill().getPathSegments().getTarget().getLibelle(),
             additionalSkillProgress.getSkill().getPathSegments().getMacroSkill().getLibelle()),
-        additionalSkillProgress.getSkill().getType().name(),
+        additionalSkillProgress.getSkill().getType(),
         additionalSkillProgress.getLevel());
   }
 }
