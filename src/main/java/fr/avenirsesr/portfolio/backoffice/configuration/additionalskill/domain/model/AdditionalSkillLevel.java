80fe77ae03f8439c159cce30940ad0aee2ff0625
bilelbanelhaq <bilel.banelhaq@gmail.com>
2025-08-27 13:18:45 +0200

    fix: remove swagger dependency from domain layer

--- src/main/java/fr/avenirsesr/portfolio/backoffice/configuration/additionalskill/domain/model/AdditionalSkillLevel.java
+++ src/main/java/fr/avenirsesr/portfolio/backoffice/configuration/additionalskill/domain/model/AdditionalSkillLevel.java
@@ -1,6 +1,3 @@
 package fr.avenirsesr.portfolio.backoffice.configuration.additionalskill.domain.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(requiredProperties = {"label", "description"})
 public record AdditionalSkillLevel(String label, String description) {}
