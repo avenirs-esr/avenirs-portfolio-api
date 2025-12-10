af4f5dd74c0c47c38d78f2c98dd1a3d2d2b4b908
Nathan <nathan.pignon79@gmail.com>
2025-12-10 13:42:49 +0100

    feat(828): fix user_id bug in indexes

--- src/main/java/fr/avenirsesr/portfolio/shared/infrastructure/configuration/CacheConfig.java
+++ src/main/java/fr/avenirsesr/portfolio/shared/infrastructure/configuration/CacheConfig.java
@@ -9,6 +9,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -17,6 +18,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 @EnableCaching
+@Profile("!test")
 public class CacheConfig {
 
   @Bean
