351f62faa527099c04b54ac7594cfb689d48a2e8
Nathan <nathan.pignon79@gmail.com>
2026-06-26 10:11:52 +0200

    fix: required properties in notification dto

--- src/main/java/fr/avenirsesr/portfolio/notification/application/adapter/dto/NotificationDTO.java
+++ src/main/java/fr/avenirsesr/portfolio/notification/application/adapter/dto/NotificationDTO.java
@@ -1,10 +1,12 @@
 package fr.avenirsesr.portfolio.notification.application.adapter.dto;
 
 import fr.avenirsesr.portfolio.notification.domain.model.enums.ENotificationType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
+@Schema(requiredProperties = {"id", "createdAt", "type", "seen"})
 public record NotificationDTO(
     UUID id,
     Instant createdAt,
