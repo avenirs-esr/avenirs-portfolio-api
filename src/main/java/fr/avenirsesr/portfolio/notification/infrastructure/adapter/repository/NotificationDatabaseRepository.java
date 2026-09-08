18e9c262ba6c7c84223768727d18aa19cdc83810
aderghan <adib.derghane@outlook.com>
2026-09-08 17:43:04 +0200

    feat(2053): update methode deleteNotification

--- src/main/java/fr/avenirsesr/portfolio/notification/infrastructure/adapter/repository/NotificationDatabaseRepository.java
+++ src/main/java/fr/avenirsesr/portfolio/notification/infrastructure/adapter/repository/NotificationDatabaseRepository.java
@@ -5,6 +5,7 @@ import fr.avenirsesr.portfolio.common.data.domain.model.PagedResult;
 import fr.avenirsesr.portfolio.common.data.domain.model.enums.EUserCategory;
 import fr.avenirsesr.portfolio.common.data.infrastructure.adapter.repository.GenericJpaRepositoryAdapter;
 import fr.avenirsesr.portfolio.notification.domain.model.Notification;
+import fr.avenirsesr.portfolio.notification.domain.model.enums.ENotificationType;
 import fr.avenirsesr.portfolio.notification.domain.port.output.repository.NotificationRepository;
 import fr.avenirsesr.portfolio.notification.infrastructure.adapter.mapper.NotificationMapper;
 import fr.avenirsesr.portfolio.notification.infrastructure.adapter.model.NotificationEntity;
@@ -12,15 +13,18 @@ import fr.avenirsesr.portfolio.notification.infrastructure.adapter.specification
 import java.util.UUID;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.DeleteSpecification;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class NotificationDatabaseRepository
     extends GenericJpaRepositoryAdapter<Notification, NotificationEntity>
     implements NotificationRepository {
+  private final NotificationJpaRepository notificationJpaRepository;
 
   public NotificationDatabaseRepository(NotificationJpaRepository jpaRepository) {
     super(jpaRepository, jpaRepository, NotificationEntity.class, NotificationMapper.INSTANCE);
+    this.notificationJpaRepository = jpaRepository;
   }
 
   @Override
@@ -42,5 +46,12 @@ public class NotificationDatabaseRepository
             .and(NotificationSpecification.hasUserCategoryNullOrEquals(userCategory))
             .and(NotificationSpecification.isNotSeen());
     return jpaSpecificationExecutor.count(specification);
+  }
+
+  @Override
+  public void deleteByTypeAndElementId(ENotificationType type, UUID elementId) {
+    DeleteSpecification<NotificationEntity> specification =
+        NotificationSpecification.ofTypeAndElement(type, elementId);
+    removeAllFromDatabase((specification));
   }
 }
