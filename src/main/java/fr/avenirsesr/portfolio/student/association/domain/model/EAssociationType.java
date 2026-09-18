050f79efef50573d7edfc94a6b426d09ef16b177
iamgreendev <56599296+BanelhaqB@users.noreply.github.com>
2026-09-18 14:09:07 +0200

    feat(associations): support the declared activity and declared experience association
    refs: #2706

--- src/main/java/fr/avenirsesr/portfolio/student/association/domain/model/EAssociationType.java
+++ src/main/java/fr/avenirsesr/portfolio/student/association/domain/model/EAssociationType.java
@@ -16,6 +16,7 @@ import lombok.Getter;
 public enum EAssociationType {
   DECLARED_ACTIVITY_TRACE(DeclaredActivity.class, Trace.class),
   DECLARED_ACTIVITY_DECLARED_SKILL(DeclaredActivity.class, DeclaredSkillProgress.class),
+  DECLARED_ACTIVITY_DECLARED_EXPERIENCE(DeclaredActivity.class, DeclaredExperience.class),
   TRACE_DECLARED_SKILL(Trace.class, DeclaredSkillProgress.class),
   TRACE_DECLARED_EXPERIENCE(Trace.class, DeclaredExperience.class),
   TRACE_DECLARED_PROGRAM(Trace.class, DeclaredProgram.class),
